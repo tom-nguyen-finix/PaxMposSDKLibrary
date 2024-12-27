15454c407fa45535afc6c1767c4e1c1114363123
Tom Nguyen <tom.nguyen@finix.com>
2024-12-26 17:00:47 -0800

    remove test target dependency

--- Package.swift
+++ Package.swift
@@ -14,10 +14,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "PaxMposSDK", path: "./Sources/PaxMposSDK.xcframework"),
-        .testTarget(
-            name: "PaxMposSDKLibraryTests",
-            dependencies: ["PaxMposSDKLibrary"]
-        ),
+        .binaryTarget(name: "PaxMposSDK", path: "./Sources/PaxMposSDK.xcframework")
     ]
 )
