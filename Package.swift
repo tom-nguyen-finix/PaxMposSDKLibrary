7396979d2e3c4e2f0aa1def82d4f7b1865f51dd4
Tom Nguyen <tom.nguyen@finix.com>
2024-12-28 00:46:05 -0800

    add PaxEasyLinkController dependency.

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "PaxMposSDKLibrary",
             targets: ["PaxMposSDK"]),
     ],
+    dependencies: [
+        .package(path: "./Sources/PaxEasyLinkController.xcframework")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
